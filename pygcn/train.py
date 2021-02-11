c8681a4fce50e923cf8ef8867c1ca27f4a4ae958
Jderlich <57108571+Jderlich@users.noreply.github.com>
2021-02-11 12:34:54 +0100

    Update train.py

--- pygcn/train.py
+++ pygcn/train.py
@@ -30,8 +30,16 @@ parser.add_argument('--hidden', type=int, default=16,
 parser.add_argument('--dropout', type=float, default=0.5,
                     help='Dropout rate (1 - keep probability).')
 
+import sys
+sys.argv=['']
+del sys
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
 
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
@@ -42,14 +50,8 @@ if args.cuda:
 adj, features, labels, idx_train, idx_val, idx_test = load_data()
 
 # Model and optimizer
-model = GCN(nfeat=features.shape[1],
-            nhid=args.hidden,
-            nclass=labels.max().item() + 1,
-            dropout=args.dropout)
-optimizer = optim.Adam(model.parameters(),
-                       lr=args.lr, weight_decay=args.weight_decay)
-
-if args.cuda:
+def make_model(args, features, labels):
+  if args.cuda:
     model.cuda()
     features = features.cuda()
     adj = adj.cuda()
@@ -57,9 +59,18 @@ if args.cuda:
     idx_train = idx_train.cuda()
     idx_val = idx_val.cuda()
     idx_test = idx_test.cuda()
+  # Model and optimizer
+  model = GCN(nfeat=features.shape[1],
+              nhid=args.hidden,
+              nclass=labels.max().item() + 1,
+              dropout=args.dropout)
+  optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
+  return model, optimizer
 
 
-def train(epoch):
+# Train the given model
+def train(epoch, model, optimizer, adj, labels, idx_train, idx_val, features):
     t = time.time()
     model.train()
     optimizer.zero_grad()
@@ -83,9 +94,44 @@ def train(epoch):
           'loss_val: {:.4f}'.format(loss_val.item()),
           'acc_val: {:.4f}'.format(acc_val.item()),
           'time: {:.4f}s'.format(time.time() - t))
+    return loss_val.item(), acc_train.item(), acc_val.item()
+
+ # to train different model
+def train_iterator(args, features, labels, adj, idx_train, idx_val):
+  #  early stopping counter
+  val_acc_list = []
+  stop_counter = 0;
+  val_loss = 1;
+  train_acc = 0;
+  eff_epoch = 0;
+
+  # instantiate the model
+  model, optimizer = make_model(args, features, labels)
+
+  # Train model
+  t_total = time.time()
+  for epoch in range(args.epochs):
+    tmp_val, train_acc, val_acc = train(epoch, model, optimizer, adj, labels, idx_train, idx_val, features)
+    val_acc_list.append(val_acc)
+    eff_epoch = epoch
+    if ( val_loss == tmp_val):
+        stop_counter += 1
+    else:
+        val_loss = tmp_val
+        stop_counter = 0
+  
+    if (stop_counter == args.earlyStop):
+        print('EARLY STOPPING at Epoch {}'.format(epoch))
+        eff_epoch = epoch
+        break
+  
+  print("Optimization Finished!")
+  print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+  return val_acc, model, val_acc_list, eff_epoch;
 
 
-def test():
+# test the given model
+def test(model,features, adj, labels, idx_test):
     model.eval()
     output = model(features, adj)
     loss_test = F.nll_loss(output[idx_test], labels[idx_test])
@@ -93,6 +139,27 @@ def test():
     print("Test set results:",
           "loss= {:.4f}".format(loss_test.item()),
           "accuracy= {:.4f}".format(acc_test.item()))
+    return acc_test.item()
+
+# for multiple runs
+def multiple_run_with_rand_split(args, features, labels, adj, idx_train, idx_val, idx_test, d = 1):
+  # load random indexes
+  # args already contained the best parameters found previously
+  idx_train, idx_val, idx_test = find_indexes(features, d)
+  _, model, _, _ = train_iterator(args, features, labels, adj, idx_train, idx_val)
+  # Testing the best model
+  acc_test = test(model,features, adj, labels, idx_test )
+  return acc_test
+
+def multiple_run_scores(args, features, labels, adj, idx_train, idx_val, idx_test, d = 1):
+  acc_test_list = []
+  for i in range(10):
+    acc_test = multiple_run_with_rand_split(args, features, labels, adj, idx_train, idx_val, idx_test, d)
+    acc_test_list.append(acc_test)
+  acc_test_array = np.array(acc_test_list)
+  print('Cora dataset')
+  print('accuracy confidence interval with mean accuracy = {:.2f}% and std = {:.4f}'.format(np.mean(acc_test_array)*100,np.std(acc_test_array) ))
+  return acc_test_list
 
 
 # Train model
